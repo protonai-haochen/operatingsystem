34cd7c833bd733d8a44ab2debd019d69a999f9ba
protonai_haochen <149346333+protonai-haochen@users.noreply.github.com>
2026-01-18 23:05:49 -0600

    Update boot.h

--- kernel/include/boot.h
+++ kernel/include/boot.h
@@ -7,7 +7,7 @@ typedef struct {
     uint64_t framebuffer_base;
     uint32_t framebuffer_width;
     uint32_t framebuffer_height;
-    uint32_t framebuffer_pitch;   // pixels per scanline
+    uint32_t framebuffer_pitch;
 } BootInfo;
 
 #endif
