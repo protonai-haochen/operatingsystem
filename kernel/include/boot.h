23b61d4ed113d5821573e78756116aeff0f6fed7
protonai_haochen <149346333+protonai-haochen@users.noreply.github.com>
2026-01-19 23:09:48 -0600

    Update boot.h

--- kernel/include/boot.h
+++ kernel/include/boot.h
@@ -3,11 +3,20 @@
 
 #include <stdint.h>
 
+// BootInfo is passed from the UEFI loader into the kernel.
+// Now includes framebuffer info + current RTC date/time at boot.
 typedef struct {
     uint64_t framebuffer_base;
     uint32_t framebuffer_width;
     uint32_t framebuffer_height;
     uint32_t framebuffer_pitch;
+
+    uint16_t year;
+    uint8_t  month;
+    uint8_t  day;
+    uint8_t  hour;
+    uint8_t  minute;
+    uint8_t  second;
 } BootInfo;
 
 #endif
